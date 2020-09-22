32a74349ad49365cf1de1a92d2d686dfe6a31e65
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-22 13:54:19 -0400

    Write test and code for Bank#open_account.

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,20 @@
 class Bank
-  attr_reader :name, :person
+  attr_reader :bank_name
+  attr_accessor :person
 
-  def initialize(name, person = nil)
-    @name = name
-    @person = person
+  def initialize(bank_name)
+    @bank_name = bank_name
+    @person = nil
     creation_msg
   end
 
   def creation_msg
-    "#{name} has been created."
+    "#{bank_name} has been created."
+  end
+
+  def open_account(customer)
+    self.person = customer
+
+    "An account has been opened for #{customer.name} with #{bank_name}."
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,11 +1,12 @@
 require './lib/bank'
+require './lib/person'
 
 describe Bank do
   subject { Bank.new('JP Morgan Chase') }
 
   describe '#init' do
     it 'has a name' do
-      expect(subject.name).to eql('JP Morgan Chase')
+      expect(subject.bank_name).to eql('JP Morgan Chase')
     end
 
     it 'does not have a person by default' do
@@ -16,6 +17,19 @@ describe Bank do
   describe '#creation_msg' do
     it 'displays a creation message' do
       expect(subject.creation_msg).to eql('JP Morgan Chase has been created.')
+    end
+  end
+
+  describe '#open_account' do
+    let(:person1) { Person.new('Joe', 1000) }
+
+    it 'adds a person to the bank' do
+      subject.open_account(person1)
+      expect(subject.person).to eql(person1)
+    end
+
+    it 'displays an account creation message' do
+      expect(subject.open_account(person1)).to eql('An account has been opened for Joe with JP Morgan Chase.')
     end
   end
 end
